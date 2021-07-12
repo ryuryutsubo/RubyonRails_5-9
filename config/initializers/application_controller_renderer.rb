8a3f55f34b5572e24eda244e67f0b3975dfaa63c
ryuryutsubo <ryuryutsubo0529@gmail.com>
2021-07-12 07:27:15 +0000

    [finish]5-9

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '14c2593858cb2ed236246688aeb0f8bb81a3c6cdfb7e576e297fd808e18ee8e5e2dca4b581f1bac6c493b659924d14485e02e34b5155fa93b2930c43384149ce'
